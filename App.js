a661c42eea07a27775c84a426dd4e5ae59a4eea5
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-07-24 13:16:06 -0300

    Tarefa 1

---
+++ App.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TouchableOpacity, 
+  TextInput
+ } from 'react-native'
+
+import { FontAwesome } from '@expo/vector-icons'
+
+export default function App(){
+  const [tarefa, setTarefa] = useState('');
+
+
+  function handleAdd(){
+    alert(tarefa)
+  }
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>Tarefas</Text>
+
+      <View style={styles.containerInput}>
+        <TextInput
+          placeholder="Digite sua tarefa..."
+          style={styles.input}
+          value={tarefa}
+          onChangeText={ (text) => setTarefa(text) }
+        />
+
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
+          <FontAwesome name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#22272e',
+    paddingTop: 28,
+  },
+  title:{
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#FFF',
+    marginTop: '5%',
+    paddingStart: '5%',
+    marginBottom: 12,
+  },
+  containerInput:{
+    flexDirection: 'row',
+    width: '100%',
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 22,
+  },
+  input:{
+    width: '75%',
+    backgroundColor: '#FBFBFB',
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
+  buttonAdd:{
+    width: '15%',
+    height: 44,
+    backgroundColor: '#73f7ff',
+    marginLeft: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4
+  }
+})
